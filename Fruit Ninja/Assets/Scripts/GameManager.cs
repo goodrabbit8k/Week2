215491f1dbee20979f308aa81cf34c7d88480a7b
Raden Agung <goodrabbit8k@gmail.com>
2023-03-23 17:05:00 +0700

    day4 part 2

--- Fruit Ninja/Assets/Scripts/GameManager.cs
+++ Fruit Ninja/Assets/Scripts/GameManager.cs
@@ -6,10 +6,14 @@ using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] Image explosionImage;
+
     public TextMeshProUGUI scoreText;
 
     KnifeManager knife;
     SpawnManager spawner;
+    FruitManager[] fruits;
+    BombManager[] bombs;
 
     int score;
 
@@ -17,6 +21,8 @@ public class GameManager : MonoBehaviour
     {
         knife = GameObject.Find("Knife").GetComponent<KnifeManager>();
         spawner = GameObject.Find("Spawn Manager").GetComponent<SpawnManager>();
+        fruits = FindObjectsOfType<FruitManager>();
+        bombs = FindObjectsOfType<BombManager>();
     }
 
     void Start()
@@ -26,8 +32,17 @@ public class GameManager : MonoBehaviour
 
     void NewGame()
     {
+        knife.enabled = true;
+        spawner.enabled = true;
+
+        Time.timeScale = 1;
+
         score = 0;
         scoreText.text = "Score : " + score.ToString();
+
+        Time.timeScale = 1;
+
+        ClearScene();
     }
 
     public void AddingScore(int fruitPoint)
@@ -35,10 +50,58 @@ public class GameManager : MonoBehaviour
         score += fruitPoint;
         scoreText.text = "Score : " + score.ToString();
     }
+
+    void ClearScene()
+    {
+        foreach (FruitManager fruit in fruits)
+        {
+            Destroy(fruit.gameObject);
+        }
+
+        foreach (BombManager bomb in bombs)
+        {
+            Destroy(bomb.gameObject);
+        }
+    }
     
     public void GameOver()
     {
         knife.enabled = false;
         spawner.enabled = false;
+
+        StartCoroutine(ExplosionCondition());
+    }
+
+    IEnumerator ExplosionCondition()
+    {
+        float elapsed = 0;
+        float duration = 0.05f;
+
+        while (elapsed < duration)
+        {
+            float time = Mathf.Clamp01(elapsed / duration);
+            explosionImage.color = Color.Lerp(Color.clear, Color.white, time);
+
+            Time.timeScale = 1 - time;
+            elapsed += Time.unscaledDeltaTime;
+
+            yield return null;
+        }
+
+        yield return new WaitForSecondsRealtime(1);
+
+        NewGame();
+
+        elapsed = 0;
+
+        while (elapsed < duration)
+        {
+            float time = Mathf.Clamp01(elapsed / duration);
+            explosionImage.color = Color.Lerp(Color.white, Color.clear, time);
+
+            elapsed += Time.unscaledDeltaTime;
+
+            yield return null;
+        }
     }
 }
