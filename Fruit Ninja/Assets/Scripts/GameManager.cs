fa2d71da34021cd73c2586bc6474292b983b05ff
Raden Agung <goodrabbit8k@gmail.com>
2023-03-24 20:46:20 +0700

    day5 finished

--- Fruit Ninja/Assets/Scripts/GameManager.cs
+++ Fruit Ninja/Assets/Scripts/GameManager.cs
@@ -6,14 +6,11 @@ using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] Image explosionImage;
-
     public TextMeshProUGUI scoreText;
+    public Image explosionImage;
 
     KnifeManager knife;
     SpawnManager spawner;
-    FruitManager[] fruits;
-    BombManager[] bombs;
 
     int score;
 
@@ -21,8 +18,6 @@ public class GameManager : MonoBehaviour
     {
         knife = GameObject.Find("Knife").GetComponent<KnifeManager>();
         spawner = GameObject.Find("Spawn Manager").GetComponent<SpawnManager>();
-        fruits = FindObjectsOfType<FruitManager>();
-        bombs = FindObjectsOfType<BombManager>();
     }
 
     void Start()
@@ -32,15 +27,10 @@ public class GameManager : MonoBehaviour
 
     void NewGame()
     {
+        Time.timeScale = 1f;
+
         knife.enabled = true;
         spawner.enabled = true;
-
-        Time.timeScale = 1;
-
-        score = 0;
-        scoreText.text = "Score : " + score.ToString();
-
-        Time.timeScale = 1;
 
         ClearScene();
     }
@@ -50,56 +40,63 @@ public class GameManager : MonoBehaviour
         score += fruitPoint;
         scoreText.text = "Score : " + score.ToString();
     }
-
-    void ClearScene()
-    {
-        foreach (FruitManager fruit in fruits)
-        {
-            Destroy(fruit.gameObject);
-        }
-
-        foreach (BombManager bomb in bombs)
-        {
-            Destroy(bomb.gameObject);
-        }
-    }
     
     public void GameOver()
     {
+        score = 0;
+        scoreText.text = "Score : " + score.ToString();
+
         knife.enabled = false;
         spawner.enabled = false;
 
         StartCoroutine(ExplosionCondition());
     }
 
+    void ClearScene()
+    {
+        FruitManager[] fruits = FindObjectsOfType<FruitManager>();
+
+        foreach (FruitManager fruit in fruits)
+        {
+            Destroy(fruit.gameObject);
+        }
+
+        BombManager[] bombs = FindObjectsOfType<BombManager>();
+
+        foreach (BombManager bomb in bombs)
+        {
+            Destroy(bomb.gameObject);
+        }
+    }
+
     IEnumerator ExplosionCondition()
     {
-        float elapsed = 0;
-        float duration = 0.05f;
+        float elapsed = 0f;
+        float duration = 0.5f;
 
         while (elapsed < duration)
         {
             float time = Mathf.Clamp01(elapsed / duration);
             explosionImage.color = Color.Lerp(Color.clear, Color.white, time);
 
-            Time.timeScale = 1 - time;
+            Time.timeScale = 1f - time;
             elapsed += Time.unscaledDeltaTime;
 
             yield return null;
         }
 
-        yield return new WaitForSecondsRealtime(1);
+        yield return new WaitForSecondsRealtime(1f);
 
         NewGame();
 
-        elapsed = 0;
+        elapsed = 0f;
 
         while (elapsed < duration)
         {
             float time = Mathf.Clamp01(elapsed / duration);
             explosionImage.color = Color.Lerp(Color.white, Color.clear, time);
 
-            elapsed += Time.unscaledDeltaTime;
+            elapsed += UnityEngine.Time.unscaledDeltaTime;
 
             yield return null;
         }
